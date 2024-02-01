0c5d33435fd5febb1651940271b551d9391dd76f
plaiaundi <plaiaundi@HZ309807.H012108.NET>
2024-02-01 10:29:14 +0100

    feature ver todos

--- src/main/GestorArboles.java
+++ src/main/GestorArboles.java
@@ -11,6 +11,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 import objetos.Arbol;
+import objetos.Habitat;
 
 public class GestorArboles {
 	final int SALIR = 0;
@@ -19,7 +20,7 @@ public class GestorArboles {
 	final int UPDATE = 3;
 	final int VISUALIZAR = 4;
 	
-	String host = "localhost"; 
+	String host = "localhost:3307"; 
 	String BBDD = "programacion";
 	String usuario = "root";
 	String contrasenia = "";
@@ -40,7 +41,7 @@ public class GestorArboles {
 				break;
 				
 			case INSERTAR:
-				Arbol arbol = new Arbol();
+				/*Arbol arbol = new Arbol();
 				System.out.println("Dame el nombre común del árbol");
 				arbol.setNombreComun(scan.nextLine());
 				
@@ -48,7 +49,7 @@ public class GestorArboles {
 				arbol.setNombreCientifico(scan.nextLine());
 				
 				System.out.println("Dime el habitat");
-				arbol.setHabitat(scan.nextLine());
+				arbol.setId_Habitat(Integer.parseInt(scan.nextLine()));
 				
 				System.out.println("Dime la altura");
 				arbol.setAltura(Integer.parseInt(scan.nextLine()));
@@ -58,20 +59,20 @@ public class GestorArboles {
 				
 				
 				
-				insertarArbol(arbol);
+				insertarArbol(arbol);*/
 				break;
 				
 			case UPDATE:
-				update();
+			//	update();
 				break;
 				
 			case ELIMINAR:
-				delete();
+			//	delete();
 				break;
 			
 			case VISUALIZAR:
-				ArrayList<Arbol> arbole = arboles();
-				visualizarArboles(arbole);
+				
+				visualizarArboles(arboles());
 				break;
 				
 			default:
@@ -84,7 +85,7 @@ public class GestorArboles {
 		
 	private ArrayList<Arbol> arboles() throws SQLException, ClassNotFoundException{
 		
-		ArrayList<Arbol> arboles = new ArrayList();
+		ArrayList<Arbol> arboles = new ArrayList<Arbol>();
 		
 		//cargar la libreria mysql conector
 		Class.forName("com.mysql.cj.jdbc.Driver");
@@ -92,18 +93,31 @@ public class GestorArboles {
 		//crear la conexion
 		Connection conexion = DriverManager.getConnection("jdbc:mysql://" + host + "/" + BBDD, usuario, contrasenia);
 		
-		//Crear el statement
+		//Crear el prepare statement
 		Statement st =  conexion.createStatement();
+				
+		ResultSet resultado = st.executeQuery("SELECT * FROM arboles2 a INNER JOIN habitat h ON a.id_habitat = h.Id");
+			
 		
-		//Ejecutar la consulta y recibir el resultado
-		ResultSet resultado= st.executeQuery("select * from arboles");
 	
 		while(resultado.next()) {
-			Arbol arbol = new Arbol();
-			arbol.setId(resultado.getInt("id"));
-			arbol.setNombreComun("nombre_comun");
 			
-			arboles.add(arbol);
+			Arbol a = new Arbol();
+			Habitat h = new Habitat();
+			
+			a.setId(resultado.getInt("id"));
+			a.setNombreComun(resultado.getString("nombre_comun"));
+			a.setNombreCientifico(resultado.getString("nombre_cientifico"));
+			a.setId_Habitat(resultado.getInt("Id_habitat"));
+			
+			h.setNombre(resultado.getString("nombre"));
+			h.setId(resultado.getInt("Id"));
+			
+			a.setAltura(resultado.getInt("altura"));
+			a.setOrigen(resultado.getString("origen"));
+			a.setFecha_encontrado(resultado.getString("fecha_encontrado"));
+
+			arboles.add(a);
 		}
 		
 		return arboles;
@@ -111,6 +125,7 @@ public class GestorArboles {
 	}
 	
 	public void visualizarArboles(ArrayList<Arbol> arboles) {
+			
 		for (Arbol arbol : arboles) {
 			System.out.println(arbol);	
 	}
@@ -126,7 +141,7 @@ public class GestorArboles {
 		System.out.println(VISUALIZAR + "- Visualizar arboles");
 	}
 	
-	public void insertarArbol(Arbol arbol) throws ClassNotFoundException, SQLException {
+	/*public void insertarArbol(Arbol arbol) throws ClassNotFoundException, SQLException {
 			Scanner scan = new Scanner(System.in);	
 			
 			
@@ -142,7 +157,7 @@ public class GestorArboles {
 				PreparedStatement pst =  conexion.prepareStatement(sql);
 				pst.setString(1, arbol.getNombreComun());
 				pst.setString(2, arbol.getNombreCientifico());
-				pst.setString(3, arbol.getHabitat());
+				pst.setInt(3, arbol.getId_Habitat());
 				pst.setInt(4, arbol.getAltura());
 				pst.setString(5, arbol.getOrigen());
 				
@@ -158,5 +173,5 @@ public class GestorArboles {
 	public void delete() {
 		
 	}
-	
+	*/
 }
--- src/objetos/Arbol.java
+++ src/objetos/Arbol.java
@@ -1,23 +1,27 @@
 package objetos;
 
+import java.util.Date;
+
 public class Arbol {
 	private int id;
 	private String nombreComun;
 	private String nombreCientifico;
-	private String habitat;
+	private int Id_habitat;
 	private int altura;
 	private String origen;
+	private String fecha_encontrado;
+	private Boolean nombre_singular;
 	
 	
 	public Arbol() {
 		
 	}
 	
-	public Arbol(int id, String nombreComun, String nombreCientifico, String habitat, int altura, String origen) {
+	public Arbol(int id, String nombreComun, String nombreCientifico, int habitat, int altura, String origen) {
 		this.id = id;
 		this.nombreComun = nombreComun;
 		this.nombreCientifico = nombreCientifico;
-		this.habitat = habitat;
+		this.Id_habitat = habitat;
 		this.altura = altura;
 		this.origen = origen;
 	}
@@ -40,11 +44,11 @@ public class Arbol {
 	public void setNombreCientifico(String nombreCientifico) {
 		this.nombreCientifico = nombreCientifico;
 	}
-	public String getHabitat() {
-		return habitat;
+	public int getId_Habitat() {
+		return Id_habitat;
 	}
-	public void setHabitat(String habitat) {
-		this.habitat = habitat;
+	public void setId_Habitat(int habitat) {
+		this.Id_habitat = habitat;
 	}
 	public int getAltura() {
 		return altura;
@@ -58,12 +62,31 @@ public class Arbol {
 	public void setOrigen(String origen) {
 		this.origen = origen;
 	}
+	
+	public String getFecha_encontrado() {
+		return fecha_encontrado;
+	}
+
+	public void setFecha_encontrado(String fecha_encontrado) {
+		this.fecha_encontrado = fecha_encontrado;
+	}
+
+	public Boolean getNombre_singular() {
+		return nombre_singular;
+	}
+
+	public void setNombre_singular(Boolean nombre_singular) {
+		this.nombre_singular = nombre_singular;
+	}
 
 	@Override
 	public String toString() {
 		return "Arbol [id=" + id + ", nombreComun=" + nombreComun + ", nombreCientifico=" + nombreCientifico
-				+ ", habitat=" + habitat + ", altura=" + altura + ", origen=" + origen + "]";
+				+ ", Id_habitat=" + Id_habitat + ", altura=" + altura + ", origen=" + origen + "]";
+	}
+
+
 	}
 	
 	
-}
+
